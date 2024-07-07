a8a7181525f77d9a7590ccc022d01d5126a638f7
NorrapatN <charge_n@hotmail.com>
2024-07-07 16:40:34 +0700

    Implement hidden sheet and data validations

--- main.js
+++ main.js
@@ -7,8 +7,9 @@ workbook.created = new Date(1985, 8, 30);
 workbook.modified = new Date();
 workbook.lastPrinted = new Date(2016, 9, 27);
 
-const sheet = workbook.addWorksheet("Test");
-sheet.columns = [
+const vehListSheet = workbook.addWorksheet("VehList", { state: "veryHidden" });
+vehListSheet.orderNo = 2;
+vehListSheet.columns = [
   {
     header: "Registration",
     key: "registration",
@@ -16,16 +17,47 @@ sheet.columns = [
   },
 ];
 
+vehListSheet.addRows([
+  { registration: "AAA-111" },
+  { registration: "AAA-222" },
+  { registration: "AAA-333" },
+  { registration: "AAA-444" },
+  { registration: "AAA-555" },
+  { registration: "AAA-666" },
+  { registration: "AAA-777" },
+  { registration: "AAA-888" },
+  { registration: "AAA-999" },
+  { registration: "AAA-000" },
+]);
+
+const sheet = workbook.addWorksheet("Test");
+sheet.orderNo = 1;
+sheet.columns = [
+  {
+    header: "Registration",
+    key: "registration",
+    width: 15,
+  },
+  {
+    header: "Plan name",
+    key: "planName",
+    width: 15,
+  },
+];
+
 const rows = sheet.getRows(2, 4);
-console.log("rows", rows);
 
 for (const row of rows) {
   const cell = row.getCell("registration");
   cell.dataValidation = {
     type: "list",
-    allowBlank: true,
-    formulae: ["AAA", "BBB"],
+    allowBlank: false,
+    formulae: [`${vehListSheet.name}!$A$2:$A$${vehListSheet.rowCount}`],
+    error: "Please select registration from the list",
+    errorTitle: "Invalid registration",
+    errorStyle: "stop",
+    showErrorMessage: true,
   };
 }
 
-workbook.xlsx.writeFile("test.xlsx");
+workbook.xlsx.writeFile("output/test.xlsx");
